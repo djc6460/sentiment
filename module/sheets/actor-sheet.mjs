7a355f8de115527c87851a5a0ecdfc8a2d729c42
David Curry <dcurry@ktlsolutions.com>
2023-12-15 19:38:45 -0500

    Fixed broken actor

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -67,10 +67,6 @@ export class SentimentActorSheet extends ActorSheet {
    * @return {undefined}
    */
   _prepareCharacterData(context) {
-    // Handle ability scores.
-    for (let [k, v] of Object.entries(context.system.abilities)) {
-      v.label = game.i18n.localize(CONFIG.BOILERPLATE.abilities[k]) ?? k;
-    }
   }
 
   /**
